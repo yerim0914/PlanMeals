c688cb2f60c8746e6c4d4062241dd72da510b12c
unknown <morri0914@hanmail.net>
2023-10-24 03:15:39 +0900

    layout 변경, collapsed

--- container/Navigation/Navigation.tsx
+++ container/Navigation/Navigation.tsx
@@ -1,13 +1,10 @@
 
 import './Navigation.css';
 import {
-    UploadOutlined,
-    UserOutlined,
-    VideoCameraOutlined,
-    MenuOutlined,
-    ArrowRightOutlined,
+  MenuUnfoldOutlined,
+  MenuFoldOutlined
   } from '@ant-design/icons';
-import { Layout, Menu, Button, theme, Typography } from 'antd';
+import { Layout, Menu, Button, theme, Typography, Collapse } from 'antd';
 
 import { useRouter } from 'next/navigation';
 import React, { ReactNode, useState } from 'react';
@@ -22,44 +19,43 @@ interface NavigationProps {
 const Navigation: React.FunctionComponent<NavigationProps> = (props) => {
   const [collapsed, setCollapsed] = useState(false);
   const router = useRouter();
-
-  const recButton: React.CSSProperties = {
-    backgroundImage: `url('../../public/utensils.svg')`
-  }
-
+  
   const setCollapsedHandler = () => {
     return collapsed === true ? setCollapsed(false): setCollapsed(true);
   }
 
   return (
       <Layout rootClassName="NavigationWrapper">
-      <div className="navigation-sider">
-      <Sider trigger={null} collapsible collapsed={collapsed} theme={'light'} style={{minWidth:"100%"}}>
+      <Sider trigger={null} collapsible collapsed={collapsed} theme={'light'} style={{minWidth:"100%", height:"100%"}}>
         <div className="nav-menu">
-          <Button type="text" style={{margin: '20px 0', width: '100%', height: '100px', fontSize: 'xx-large', fontWeight: '800', color: 'rgb(13, 13, 43)'}}
+        <Button
+            className='nav-fold-button'
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+          />
+          <Button type="text" style={{margin: '20px 0', width: '100%', height: '100px', fontSize: 'xx-large', fontWeight: '800', color: 'rgb(13, 13, 43)', display: "inline"}}
             onClick={() => {router.push('./')}}
           >
-            냉장고를 <br />
-            부탁해
+            {collapsed ? "냉부" : <span className="multi-line-text">냉장고를 부탁해</span>}
           </Button>
           <Button
             type="text"
             onClick={() => {router.push('./recommend')}}
-            className="navbtn recommend-btn"
+            className={collapsed ? "navbtn" : "navbtn recommend-btn"}
           />
           <Button
             type="text"
             onClick={() => {router.push('./ingredient')}}
-            className="navbtn ingredient-btn"
+            className={collapsed ? "navbtn": "navbtn ingredient-btn"}
           />
           <Button
             type="text"
             onClick={() => {router.push('./mypage')}}
-            className="navbtn mypage-btn"
+            className={collapsed ? "navbtn": "navbtn mypage-btn"}
           />
         </div>
       </Sider> 
-      </div>
       <Layout>
         <Content
           className='content-wrapper'
